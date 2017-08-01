7afe827e5471fee12034304aeac27458d789ff23
Ahmad Arshad <muji786@users.noreply.github.com>
2017-08-01 17:05:39 -0400

    Assignment 6

---
+++ Assignment6/README.md
@@ -0,0 +1,9 @@
+# Assignment 6
+Make a physical controller or output for something you have made in p5.
+
+I have tried to combine my first p5 assignment and my last arduino experiment of a light sensor.  I take a video stream and implement sort of a motion detection system. I was having difficulties comparing frames, so I loadpixel() of each frame as an image and copy it to a new array and compare them every single frame. Needless to say its a performance hit. If there are changes in rgb+opacity of any pixel, I send a signal down to my arduino that turns the alarm on. mp4 attached.
+
+## Observation assignment:
+Write on your journal a paragraph or two about what computing means to you at this point. Is it adding something to your life? Is it helping you become a better person? What are you getting out of it, what do others get from it?
+
+Having been in the IT field for over 18 years, computing is no more than a tool to me to solve problems around me. Even though it has become synonymous to progress, something human is lost with overreliance on it. In my opinion, it has created more problems where people are connected digitally via social media but dont seem to have any more real interactions. It has made me a worse person, as I feel addicted to Twitter etc and cant close it to appreciate the smiles around me. My trip to lake george was a shining example, where members of family were more concerned about taking pictures and sharing them on the facebook rather than enjoying the moment and capturing it on the camera of one's memory.
---
+++ Assignment6/empty-example/sketch.js
@@ -0,0 +1,76 @@
+var serial;
+var portName = '/dev/cu.usbmodem1421'; // your serial port goes here
+var mpos = 0;
+var inByte;
+var video;
+var oldFrame = [];
+var movement = 0;
+
+function setup() {
+    createCanvas(10, 10);
+    pixelDensity(1); //older displays
+    video = createCapture(VIDEO); // creates a video dom element that connects to the camera
+    video.hide(); //hides the actual video dom element and only shows canvas
+
+    serial = new p5.SerialPort(); // make a new instance of the serialport library
+    serial.on('list', printList); // set a callback function for the serialport list event
+    serial.on('connected', serverConnected); // callback for connecting to the server
+    serial.on('open', portOpen); // callback for the port opening
+    serial.on('data', serialEvent); // callback for when new data arrives
+    serial.on('error', serialError); // callback for errors
+    serial.on('close', portClose); // callback for the port closing
+    serial.open(portName); // open a serial port
+}
+
+function draw() {
+    background(0);
+    image(video, 0, 0, width, height);
+    filter(THRESHOLD);
+    loadPixels();
+    arrayCopy(pixels, oldFrame);
+    for (var i = 0; i < pixels.length; i++) {
+        for (var j = 0; j < oldFrame.length; j++) {
+            if (pixels[i] != oldFrame[j])
+                movement++;
+        }
+    }
+    if (movement > 40000) {
+        serial.write(1);
+        console.log('Movement Detected... ' + movement);
+        movement = 0;
+    }
+}
+
+//function draw() {
+//    background(0);
+//    fill(255);
+//    ellipse(mouseX, height / 2, 20, 20);
+//    textSize(36);
+//    text("Outgoing number: " + mpos, 20, height - 60);
+//}
+
+function serialError(err) {
+    console.log('Something went wrong with the serial port. ' + err);
+}
+
+function portClose() {
+    console.log('The serial port closed.');
+}
+
+function printList(portList) {
+    for (var i = 0; i < portList.length; i++) {
+        console.log(i + " " + portList[i]);
+    }
+}
+
+function serialEvent() {
+    inByte = Number(serial.read()); // stores the data from the Arduino
+}
+
+function serverConnected() {
+    console.log('connected to server.');
+}
+
+function portOpen() {
+    console.log('the serial port opened.')
+}
